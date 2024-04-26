3ab7ce2323cedb94adca506bd8f46f15851ac7c3
naliali <nalialiangela365@gmail.com>
2024-04-26 09:39:25 +0300

    initial commit

--- src/__tests__/user.test.js
+++ src/__tests__/user.test.js
@@ -2,6 +2,6 @@ import { username, city, image } from "../data/user";
 
 test("username, city, and image are all exported", () => {
   expect(username).toEqual("Liza");
-  expect(city).toEqual("New York");
+  expect(city).toEqual("NewYork");
   expect(image).toEqual("https://i.imgur.com/mV8PQxj.gif");
 });
